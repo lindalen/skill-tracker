4ada7236c7229cb4de15704a7c1f2343bade0c09
lindalen <43276209+lindalen@users.noreply.github.com>
2021-01-05 19:29:22 +0100

    Minor update
    - Adds components Timer and ProgressBar
    - Imports Timer and ProgressBar

--- src/SkillSection.tsx
+++ src/SkillSection.tsx
@@ -1,11 +1,21 @@
 import React, {useState} from "react";
+import "./SkillSection.css"
 
+import {Timer} from "./Timer";
+import {ProgressBar} from "./ProgressBar";
+/**
+ * Component Description
+ * Data - total time spent on a skill, current timer value, goal time
+ * Functionality - start timer, stop timer, display current timer value, display goal value
+ * Sub-Components - Timer
+ */
 const SkillSection : React.FC = () => {
     const [time, setTime] = useState(0);
-
+    const [goal, setGoal] = useState(0);
     return (
-        <div>
-            {time}
+        <div className="skill-section">
+            <Timer/>
+            <ProgressBar/>
         </div>
     );
 }
---
+++ src/Timer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface TimerProps {
+
+}
+
+export const Timer: React.FC<TimerProps> = ({}) => {
+        return (<div></div>);
+}
