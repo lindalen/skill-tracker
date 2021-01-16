809901a4de950ec94696a908519f811ee8ea093a
lindalen <43276209+lindalen@users.noreply.github.com>
2021-01-16 15:22:35 +0100

    Refactor content of settings div
    Description:
    - The content of the "settings" div is now its own variable. This made it easier to have identical content in "settings show" and "settings hide".

--- src/SkillSettings.tsx
+++ src/SkillSettings.tsx
@@ -6,18 +6,28 @@ interface SkillSettingsProps {
 }
 
 export const SkillSettings: React.FC<SkillSettingsProps> = ({show}) => {
+        const settingsContent = 
+            <>
+                Skill: <input type="text" name="skill-name" value="x" placeholder="Enter new skillname here"/>
+                Goal: 
+                <div className="threeByOne"> 
+                    <input className="cell" type="number" name="goal-hour" value="0" placeholder="hours"/>
+                    <input className="cell" type="number" name="goal-min" value="0" placeholder="minutes"/>
+                    <input className="cell" type="number" name="goal-sec" value="0" placeholder="seconds"/>
+                </div>
+                CHANGE SKILL NAME
+                CHANGE COLOR
+            </>
+        
         const settings = 
-        <div className="settings show">
-            SETTINGS HERE
-            CHANGE GOAL
-            CHANGE SKILL NAME
-            CHANGE COLOR
-        </div>
+            <div className="settings show">
+                {settingsContent}
+            </div>
 
         const fadingSettings = 
-        <div className="settings hide">
-            bye bye
-        </div>
+            <div className="settings hide">
+                {settingsContent}
+            </div>
         return (
             <div>
             
